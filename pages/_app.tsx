65bc614a932ae2fa1a5568948d0c84e6a20849ca
Alexandre Ferreira Martins <zedmaster@gmail.com>
2021-03-08 21:25:42 -0300

    Correçao da estrutura

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,53 @@
 import '../styles/globals.css'
 import '../styles/App.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "next/app";
+import Head from "next/head";
+
+
+export default class MyApp extends App {
+
+ 
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <React.Fragment>
+        <Head>
+          <link rel="shortcut icon" href="/logo512.png" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />      
+          
+          <meta property="og:locale" content="pt_BR" />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content="Atelie Mininax || Costura || Artesanato || Curitiba - PR"/>
+          <meta property="og:description" content="O Ateliê Mininax trabalha com artesanato e costura em Curitiba - Barreirinha"/>
+          <meta property="og:url" content="https://mininax.com.br"/>
+          <meta property="og:site_name" content="Mininax"/>
+
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Atelie Mininax || Costura || Artesanato || Curitiba - PR" />
+          <meta name="twitter:description" content="O Ateliê Mininax trabalha com artesanato e costura em Curitiba - Barreirinha" />
+          <link rel="canonical" href="https://mininax.com.br" /> 
+          <title>Mininax</title>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-W5KW36HEMD"></script>
+          <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-W5KW36HEMD');
+                  `,
+                }}
+          />
+        </Head>
+        <Component {...pageProps} />
+     </React.Fragment>
+    )
+  }
 }
 
-export default MyApp
+//export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,73 +6,42 @@
 export default function Home() {
   return (
     
-    <div>
-    <head>
-      
-      <link rel="icon" href="/logo512.png" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />      
-      
-      <meta property="og:locale" content="pt_BR" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Atelie Mininax || Costura || Artesanato || Curitiba - PR"/>
-      <meta property="og:description" content="O Ateliê Mininax trabalha com artesanato e costura em Curitiba - Barreirinha"/>
-      <meta property="og:url" content="https://mininax.com.br"/>
-      <meta property="og:site_name" content="Mininax"/>
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Atelie Mininax || Costura || Artesanato || Curitiba - PR" />
-      <meta name="twitter:description" content="O Ateliê Mininax trabalha com artesanato e costura em Curitiba - Barreirinha" />
-      <link rel="canonical" href="https://mininax.com.br" /> 
-      <title>Mininax</title>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-W5KW36HEMD"></script>
-      <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-W5KW36HEMD');
-              `,
-            }}
-      />
-  </head>
-  <body>
-  <div className="App">
-      <header className="App-header">
-        <img src="/logo.webp" className="App-logo" alt="logo" />
-        <p>
-          Opa!! O site ainda não esta pronto, enquanto isto curta nossas redes sociais. Valeu!!
-        </p>
-        <a
-          className="App-link"
-          href="https://www.instagram.com/atelie.mininax/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Instagram
-        </a>
-        <a
-          className="App-link"
-          href="https://www.facebook.com/ateliemininax-111346864341195"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Facebook
-        </a>
-        <a
-          className="App-link"
-          href="tel:41998109882"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          41 998109882
-        </a>
-      </header>
-    </div>
-    
-  </body>
-
+  <div>
+      <body>
+        <div className="App">
+          <header className="App-header">
+            <img src="/logo.webp" className="App-logo" alt="logo" />
+            <p>
+              Opa!! O site ainda não esta pronto, enquanto isto curta nossas redes sociais. Valeu!!
+            </p>
+            <a
+              className="App-link"
+              href="https://www.instagram.com/atelie.mininax/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              className="App-link"
+              href="https://www.facebook.com/ateliemininax-111346864341195"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+            <a
+              className="App-link"
+              href="tel:41998109882"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              41 998109882
+            </a>
+          </header>
+        </div>
+        
+      </body>
 
     </div>
   )
